76b1fe4c6ea081e7a2dadd878d0cd9b0af323f83
Petronio <petroniorocha81@gmail.com>
2021-12-08 21:39:24 -0300

    Atualização da pasta

---
+++ Python - Introduction to Data Science/Python library - Scipy/part - 1.py
@@ -0,0 +1,10 @@
+import scipy as sp
+from scipy import integrate
+from scipy import cluster
+from scipy import fftpack
+
+help(integrate)  # informa as informações do subpacote
+
+help()  # dessa maneira, você pode digitar o subpacote a qual quer as informações
+
+sp.info(fftpack)  # nos retorna as informações sobre nosso subpacote
